85e0744e908c5218207b5eaf7b29abe369d69f98
lesapin <47056259+lesapin@users.noreply.github.com>
2024-01-11 15:43:36 +0200

    Update logcompressor.sh

--- logs/logcompressor.sh
+++ logs/logcompressor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# select all logs that are above the size threshold of an "empty" file and save
-# them in an archive with their names simplified to l%day-of-month%hour-of-day.log
+# at the beginning of the month, select all logs that are above the size threshold of an "empty" file 
+# and save them in an archive with their names simplified to l%day-of-month%hour-of-day.log
 
 if [ $# -lt 1 ]; then
     echo "usage: $0 --min-filesize [kilobytes]"
 elif [ $# -eq 2 ]; then
     cd "$(dirname "$0")"
     if [ $1 = "--min-filesize" ]; then
-        FLDR="$(date +%G%b)"
+        FLDR="$(date -d '$(date) 1 month ago' +%G%b)"
         if ! [ -d $FLDR ]; then
             mkdir $FLDR
         fi
