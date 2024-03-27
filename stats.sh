c8185f1b3f4dd59442d40db27052ded17669fd53
lesapin <47056259+lesapin@users.noreply.github.com>
2024-03-28 01:07:15 +0200

    Add files via upload

--- stats.sh
+++ stats.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Read .stat files output by ServerMon plugin. Parse them into a
+# gnuplot .dat file. Use gnuplot to create statistics of server activity.
+
 args=("$@")
 title=${args[0]}
 filename=${title// /_}
@@ -12,6 +15,7 @@ LOGS=`ls *.stats`
 DATFILE="server.dat"
 IMGFILE="${filename}.png"
 
+# print .dat header
 printf "# gnuplot datfile - %s\n" $title > $DATFILE
 printf "# DATE\tMANHOURS\tACTIVEHOURS\tMAXCLIENTS\tUNIQUECLIENTS\tCONNECTIONS\n" >> $DATFILE
 
@@ -20,9 +24,11 @@ mkdir $foldername
 for log in $LOGS;
 do
 	nLogs=$((nLogs+1))
-
+	
+	# Date is used as the key
 	printf "${log:5:4}\t" >> $DATFILE
 
+	# Parse all lines except individual player activity
 	while IFS=' ' read -ra line
 	do
 		if [[ "${line[0]}" != PLAYER ]]; then
