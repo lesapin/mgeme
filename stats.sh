c8851c07396d23437a664c06f1e04ce2062dab64
lesapin <47056259+lesapin@users.noreply.github.com>
2024-03-28 04:37:23 +0200

    Add files via upload

--- stats.sh
+++ stats.sh
@@ -6,26 +6,28 @@
 args=("$@")
 title=${args[0]}
 filename=${title// /_}
+
+LOGS=`ls *.stats`
+DATFILE="${filename}.dat"
+IMGFILE="${filename}.png"
 foldername=`date +%b%g`
-nLogs=0
 
 IFS=$'\n'
 
-LOGS=`ls *.stats`
-DATFILE="server.dat"
-IMGFILE="${filename}.png"
+mkdir -p $foldername
 
 # print .dat header
 printf "# gnuplot datfile - %s\n" $title > $DATFILE
 printf "# DATE\tMANHOURS\tACTIVEHOURS\tMAXCLIENTS\tUNIQUECLIENTS\tCONNECTIONS\n" >> $DATFILE
 
-mkdir $foldername
+nLogs=0
 
+# Parse log files
 for log in $LOGS;
 do
 	nLogs=$((nLogs+1))
 	
-	# Date is used as the key
+	# Date is used as x-axis key
 	printf "${log:5:4}\t" >> $DATFILE
 
 	# Parse all lines except individual player activity
@@ -39,3 +41,28 @@ do
 	printf '\n' >> $DATFILE
 done
 
+# Plot
+gnuplot <<- EOF
+set title "$title" tc rgb 0xffffff
+set tics font ",9"
+set key outside tc rgb 0xffffff
+set border lc rgb 0xffffff
+
+set xlabel "Date" tc rgb 0xffffff
+set xdata time
+set timefmt "%m%d"
+set xtics format "%d." time
+
+set ylabel "Man-hours\n[h]" tc rgb 0xffffff
+set ytics nomirror tc "yellow"
+
+set y2label "Unique Clients" tc rgb 0xffffff
+set y2range [0 : 100]
+set y2tics nomirror tc rgb 0xb7f8f1 0,5
+
+set term png size 1200,600 transparent truecolor
+set output "$IMGFILE"
+
+plot "$DATFILE" u 1:(column(2)/60/60) t 'Man-hours' w lines lc "yellow", \
+     "$DATFILE" u 1:5 axes x1y2 t 'Unique Clients' w boxes lc rgb 0xb7f8f1
+EOF
