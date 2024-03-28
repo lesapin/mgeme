a5e814793a651a0746af4d8a1fbaa9dc6fdce91d
lesapin <47056259+lesapin@users.noreply.github.com>
2024-03-28 06:40:14 +0200

    Add files via upload

--- stats.sh
+++ stats.sh
@@ -21,6 +21,9 @@ printf "# gnuplot datfile - %s\n" $title > $DATFILE
 printf "# DATE\tMANHOURS\tACTIVEHOURS\tMAXCLIENTS\tUNIQUECLIENTS\tCONNECTIONS\n" >> $DATFILE
 
 nLogs=0
+nConnections=0
+max_clients=0
+active_hours=0
 
 # Parse log files
 for log in $LOGS;
@@ -36,33 +39,55 @@ do
 		if [[ "${line[0]}" != PLAYER ]]; then
 			printf "${line[1]}\t" >> $DATFILE
 		fi
+		if [[ "${line[0]}" == ACTIVEHOURS ]]; then
+			active_hours=$((active_hours+${line[1]}))
+		fi
+		if [[ "${line[0]}" == MAXCLIENTS ]]; then
+			max_clients=$((max_clients+${line[1]}))
+		fi
+		if [[ "${line[0]}" == CONNECTIONS ]]; then
+			nConnections=$((nConnections+${line[1]}))
+		fi
 	done < $log
 
 	printf '\n' >> $DATFILE
 done
 
+avg_maxclients=$((max_clients/nLogs))
+avg_activity=$(((active_hours/nLogs)/60/60))
+avg_empty=$((24-avg_activity))
+
 # Plot
 gnuplot <<- EOF
-set title "$title" tc rgb 0xffffff
-set tics font ",9"
-set key outside tc rgb 0xffffff
-set border lc rgb 0xffffff
+	set title "$title" tc rgb 0xffffff
+	set tics font ",9"
+	set key outside tc rgb 0xffffff
+	set border lc rgb 0xffffff
 
-set xlabel "Date" tc rgb 0xffffff
-set xdata time
-set timefmt "%m%d"
-set xtics format "%d." time
+	set xlabel "Date" tc rgb 0xffffff
+	set xdata time
+	set timefmt "%m%d"
+	set xtics format "%d." time
 
-set ylabel "Man-hours\n[h]" tc rgb 0xffffff
-set ytics nomirror tc "yellow"
+	set ylabel "Sum of every clients playtime\n[h]" tc rgb 0xffffff
+        set yrange [0 : 50]
+	set ytics nomirror tc "yellow"
 
-set y2label "Unique Clients" tc rgb 0xffffff
-set y2range [0 : 100]
-set y2tics nomirror tc rgb 0xb7f8f1 0,5
+	set y2label "Number of unique clients" tc rgb 0xffffff
+	set y2range [0 : 100]
+	set y2tics nomirror tc rgb 0xb7f8f1 0,5
 
-set term png size 1200,600 transparent truecolor
-set output "$IMGFILE"
+	set term png size 1280,620 transparent truecolor
+	set output "$IMGFILE"
 
-plot "$DATFILE" u 1:(column(2)/60/60) t 'Man-hours' w lines lc "yellow", \
-     "$DATFILE" u 1:5 axes x1y2 t 'Unique Clients' w boxes lc rgb 0xb7f8f1
+	set label 1 at screen 0.848,0.8  \
+	    "Activity (avg/day)\n active: $avg_activity hours\n empty: $avg_empty hours" \
+            tc rgb 0xffffff
+
+	set label 2 at screen 0.848,0.65 "Connections\n $nConnections" tc rgb 0xffffff
+
+	set label 3 at screen 0.848,0.55 "MaxClients (avg/day)\n $avg_maxclients" tc rgb 0xffffff
+
+	plot "$DATFILE" u 1:(column(2)/60/60) t 'man-hours' w lines lc "yellow", \
+     	     "$DATFILE" u 1:5 axes x1y2 t 'unique clients' w boxes lc rgb 0xb7f8f1
 EOF
